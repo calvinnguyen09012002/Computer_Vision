aef0fcea15507f7647af6ef16a05c1320a0c2130
calvinnguyen09012002 <nguyengiaphuc09012002@gmail.com>
2023-10-10 11:35:30 +0700

    update

--- Bai2/main.py
+++ Bai2/main.py
@@ -1,7 +1,7 @@
 #Thao Tác Với Ảnh Số - Đọc , Hiển Thị, Lưu Ảnh trong OpenCV và Python
 import cv2 
 
-path = r'C:\LaptrinhC\Test_python\Bai1\line.jpg'
+path = r'C:\LaptrinhC\Computer_Vision\Bai2\line.jpg'
 
 img = cv2.imread(path)
 
