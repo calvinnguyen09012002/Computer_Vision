edebc748fdc9636a7b929c1f8e20f6a252f132cf
calvinnguyen09012002 <nguyengiaphuc09012002@gmail.com>
2023-10-10 11:50:01 +0700

    update 10/10/2023

--- Bai2/main.py
+++ Bai2/main.py
@@ -1,11 +1,19 @@
-#Thao Tác Với Ảnh Số - Đọc , Hiển Thị, Lưu Ảnh trong OpenCV và Python
+#Đọc Ảnh, Hiển Thị Và Lưu Ảnh OpenCV Python
 import cv2 
+import os
 
-path = r'C:\LaptrinhC\Computer_Vision\Bai2\line.jpg'
+path = r'C:\Visual Studio Code\Computer_Vision\Bai2\line.jpg'
+path_1 = r'C:\Visual Studio Code\Computer_Vision\Bai2'
 
 img = cv2.imread(path)
 
-cv2.imshow('Cuaso1', img)
+cv2.imshow('Image', img)
+
+os.chdir(path_1)
+
+fileName = 'New_Image.jpg'
+cv2.imwrite(fileName, img)
+print('thanh cong')
 
 cv2.waitKey(0) 
 cv2.destroyAllWindows()
